302c260edae4b84dd532b9e4e57252978ca2c9b6
Pedro Rios <PRios731@gmail.com>
2025-01-28 13:30:44 -0300

    Project created

---
+++ src/test/java/com/alt3rs/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alt3rs.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
