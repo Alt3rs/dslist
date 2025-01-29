d99ce10ecd06b4a2cf17ca1de35f32c489e4fde8
Pedro Rios <PRios731@gmail.com>
2025-01-29 13:34:26 -0300

    GET game by id

--- src/main/java/com/alt3rs/dslist/services/GameService.java
+++ src/main/java/com/alt3rs/dslist/services/GameService.java
@@ -1,10 +1,12 @@
 package com.alt3rs.dslist.services;
 
+import com.alt3rs.dslist.dto.GameDto;
 import com.alt3rs.dslist.dto.GameMinDto;
 import com.alt3rs.dslist.entities.Game;
 import com.alt3rs.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,6 +16,14 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDto findById(Long id){
+        Game result = gameRepository.findById(id).get();
+        GameDto dto = new GameDto(result);
+        return dto;
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDto> findAll(){
        List<Game> result = gameRepository.findAll();
        List<GameMinDto> dto = result.stream().map(x -> new GameMinDto(x)).toList();
